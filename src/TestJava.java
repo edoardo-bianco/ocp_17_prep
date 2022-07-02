13d663f18b94da9a5ccf5ee8b89f46f8298afb51
Edoardo Bianco <edoardo.bianco@gmail.com>
2022-07-02 08:58:33 -0300

    java class template for testes

---
+++ src/TestJava.java
@@ -0,0 +1,11 @@
+package ocp_17_prep.src;
+//Add any necessary import statement here
+public class TestJava {
+    public static void main(String [] args){
+        //add teste code here
+
+        //add any print statement here
+        System.out.println("Hello world: OCP 17");
+
+    }
+}
